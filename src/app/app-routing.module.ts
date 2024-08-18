b99547b43d9ed8151c9dde2b370222a87d858659
NaadaMahmoud <119589202+NaadaMahmoud@users.noreply.github.com>
2024-08-18 16:02:26 +0300

    first

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,15 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { NavbarComponent } from './components/navbar/navbar.component';
+import { SliderComponent } from './components/slider/slider.component';
+import { AboutusComponent } from './components/aboutus/aboutus.component';
+import { SolutionsComponent } from './components/solutions/solutions.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', component: SliderComponent },
+  { path: 'about-us', component: AboutusComponent },
+  { path: 'solutions', component: SolutionsComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,28 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './components/navbar/navbar.component';
+import { SliderComponent } from './components/slider/slider.component';
+import { TrustedByComponent } from './components/trusted-by/trusted-by.component';
+import { AboutusComponent } from './components/aboutus/aboutus.component';
+import { SolutionsComponent } from './components/solutions/solutions.component';
+import { RefrencesComponent } from './components/refrences/refrences.component';
+import { ContactUsComponent } from './components/contact-us/contact-us.component';
+import { FooterComponent } from './components/footer/footer.component';
+import { HomeComponent } from './components/home/home.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    SliderComponent,
+    TrustedByComponent,
+    AboutusComponent,
+    SolutionsComponent,
+    RefrencesComponent,
+    ContactUsComponent,
+    FooterComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
