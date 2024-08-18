89616b5e117cc2d5519cfd60aae5bde66baa5c05
NaadaMahmoud <nadamahmoudd843@gmail.com>
2024-08-18 21:21:46 +0300

    new

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,14 +1,19 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { NavbarComponent } from './components/navbar/navbar.component';
-import { SliderComponent } from './components/slider/slider.component';
+import { HomeComponent } from './components/home/home.component';
 import { AboutusComponent } from './components/aboutus/aboutus.component';
 import { SolutionsComponent } from './components/solutions/solutions.component';
+import { RefrencesComponent } from './components/refrences/refrences.component';
+import { ContactUsComponent } from './components/contact-us/contact-us.component';
 
 const routes: Routes = [
-  { path: '', component: SliderComponent },
-  { path: 'about-us', component: AboutusComponent },
+  { path: '', redirectTo: '/home', pathMatch: 'full' },
+  { path: 'home', component: HomeComponent },
+  { path: 'aboutus', component: AboutusComponent },
   { path: 'solutions', component: SolutionsComponent },
+  { path: 'refrences', component: RefrencesComponent },
+  { path: 'contactus', component: ContactUsComponent },
+  { path: '**', redirectTo: '/home' }
 ];
 
 @NgModule({
