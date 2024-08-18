7ace779bb4edf2a5a477a23e575edd58d7766fb3
NaadaMahmoud <nadamahmoudd843@gmail.com>
2024-08-18 21:04:14 +0300

    update

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,16 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import AOS from 'aos';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'business-engines';
+export class AppComponent implements OnInit {
+  ngOnInit() {
+    AOS.init({
+      duration: 1200,
+    });
+  }
 }
+
